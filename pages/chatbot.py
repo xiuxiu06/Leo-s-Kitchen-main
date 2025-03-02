ace9f8fd439c6502f65a207d5748715c657cd5ca
Tam Le <151154605+xiuxiu06@users.noreply.github.com>
2025-03-01 20:50:58 -0500

    Update chatbot.py

--- pages/chatbot.py
+++ pages/chatbot.py
@@ -1,41 +1,29 @@
 import streamlit as st
 from openai import OpenAI
 
-st.title("ChatGPT-like clone")
+st.title("Awesome Chatbot")
 
-# Set OpenAI API key from Streamlit secrets
 client = OpenAI(api_key=st.secrets["OPENAI_API_KEY"])
 
-# Set a default model
-if "openai_model" not in st.session_state:
-    st.session_state["openai_model"] = "gpt-3.5-turbo"
-
-# Initialize chat history
+# Chat History
 if "messages" not in st.session_state:
     st.session_state.messages = []
 
-# Display chat messages from history on app rerun
 for message in st.session_state.messages:
     with st.chat_message(message["role"]):
         st.markdown(message["content"])
 
-# Accept user input
-if prompt := st.chat_input("What is up?"):
-    # Add user message to chat history
-    st.session_state.messages.append({"role": "user", "content": prompt})
-    # Display user message in chat message container
+prompt = st.chat_input("Say something")
+if prompt:
     with st.chat_message("user"):
         st.markdown(prompt)
-# Display assistant response in chat message container
+    st.session_state.messages.append({"role": "user", "content": prompt})
+
     with st.chat_message("assistant"):
         stream = client.chat.completions.create(
-            model=st.session_state["openai_model"],
-            messages=[
-                {"role": m["role"], "content": m["content"]}
-                for m in st.session_state.messages
-            ],
-            stream=True,
+            model="gpt-4o",
+            messages=st.session_state.messages,
+            stream=True
         )
-
         response = st.write_stream(stream)
     st.session_state.messages.append({"role": "assistant", "content": response})
